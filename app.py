1637d1682339b4cda4d00a829ef9a3923e7b3f73
Hali <25pchen@student.aisb.hu>
2024-10-14 01:22:41 +0200

    Initial fetching of API

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
-
+import requests
+import json 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Brawl Stars Map Tracker")
+
+res = requests.get('https://api.brawlify.com/v1/events')
+response = json.loads(res.text)
+print(response)
